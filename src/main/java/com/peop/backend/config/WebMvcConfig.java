cee4aeab80ff5a70303f151330a795534c8ba5d8
praneethpj <pubudupraneeth@gmail.com>
2021-11-21 11:40:11 +0530

    aa

--- src/main/java/com/peop/backend/config/WebMvcConfig.java
+++ src/main/java/com/peop/backend/config/WebMvcConfig.java
@@ -12,15 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
-
-    @Value("${app.cors.allowedOrigins}")
-    private String[] allowedOrigins;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
-                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-                .maxAge(MAX_AGE_SECS);
-    }
+//
+//    @Value("${app.cors.allowedOrigins}")
+//    private String[] allowedOrigins;
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins(allowedOrigins)
+//                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+//                .maxAge(MAX_AGE_SECS);
+//    }
 }
